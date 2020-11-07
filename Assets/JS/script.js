8b4c55562decf1b295a808169a9e31cc2bd6d6e9
Ashvini Thomas <ashvinithomas@gmail.com>
2020-11-07 17:50:22 -0500

    added today's date using moment to planner

--- Assets/JS/script.js
+++ Assets/JS/script.js
@@ -1,10 +1,2 @@
-$(document).ready(function() {
-
-    //variables
-    var today = moment().format();
-
-
-
-
-})
-
+//Display current date in the p tag with an id of currentDay
+$('#currentDay').text(moment().format('dddd, MMMM Do, YYYY'));
