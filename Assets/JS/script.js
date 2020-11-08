e3ce6d4d65c1996c543a62f4bdb9fe3c65c5a233
Ashvini Thomas <ashvinithomas@gmail.com>
2020-11-07 19:08:59 -0500

    created if else statement to add the classes of past, future, and present

--- Assets/JS/script.js
+++ Assets/JS/script.js
@@ -3,9 +3,26 @@ $('#currentDay').text(moment().format('dddd, MMMM Do, YYYY'));
 
 console.log(moment().format('hh'));
 
-//variables
-var now = moment().format('hh');
+//variable for the military hour right now
+var now = moment().format('HH');
 
+//if var now is greater than the value of hour class
+if (now > $('.hour').val()) {
 
+    //add the class of past to the input tag
+    $('input').addClass("past");
+
+//else if var now is equal to the value of hour class  
+} else if (now == $('hour').val()) {
+
+    //add the class of present to the input tag
+    $('input').addClass("present");
+
+//if var now is less than the value of hour class
+} else {
+
+    //add the class of future to the input tag
+    $('input').addClass("future");
+};
 
 
